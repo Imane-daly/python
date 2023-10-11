79a73b105b152f418ac22aa7ac25f90d3be5e690
DL_imane <147096823+Imane-daly@users.noreply.github.com>
2023-10-11 23:20:00 +0100

    Add files via upload

---
+++ Category_child.py
@@ -0,0 +1,9 @@
+N=input("please enter the child's age:")
+if N == 6 and N == 7:
+    print("child's category is chick:")
+elif N == 8 and N == 8:
+    print("child's category is ward:")
+elif N == 10 and N == 11:
+    print("child's category is minimal:")
+else:
+    print("child's category is junior:")
