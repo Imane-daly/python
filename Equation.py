88932dd46348df94115036069d0f8d2cc106cbec
DL_imane <147096823+Imane-daly@users.noreply.github.com>
2023-10-11 23:01:03 +0100

    Delete Equation.py

--- Equation.py
+++
@@ -1,6 +0,0 @@
-age=int(input("please enter the age of the resident:"))
-sexe=input("please enter the sexe of the resident(femme/homme):")
-if (age >= 20 and sexe == "homme") or (age >= 18 and age <= 35 and sexe == "femme"):
-    print("the inhabitant is impossible")
-else:
-    print("the inhabitant and not impossible")
